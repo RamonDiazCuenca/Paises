83ce6f88b1f62ac314c3aa30af51704040382eb3
RamonDiazCuenca <ramon.dc92@gmail.com>
2021-12-15 17:51:56 +0100

    Práctica 10: Persistencia y uso preferencias

--- app/src/main/java/com/rdc/paises/PaisesFragment.java
+++ app/src/main/java/com/rdc/paises/PaisesFragment.java
@@ -1,14 +1,17 @@
 package com.rdc.paises;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.rdc.paises.placeholder.PlaceholderContent;
 
@@ -53,14 +56,31 @@ public class PaisesFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_paises_list, container, false);
 
+        // obtener preferencias en una variable llamada prefs
+        SharedPreferences prefs = getActivity().getSharedPreferences("com.rdc.paises_preferences", Context.MODE_PRIVATE);
+
         // Set the adapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
+
+            // asignar a esta variable el valor de la propiedad tipo_visualizacion
+            String tipoVisualizacion = prefs.getString("tipo_visualizacion","No hay información");
+
+            if (tipoVisualizacion.equals("listado")) {
                 recyclerView.setLayoutManager(new LinearLayoutManager(context));
             } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+                recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
+            }
+
+            // asignar a esta variable el valor almacenado en la propiedad linea
+            boolean useDivider = prefs.getBoolean("linea",false);
+
+            if(useDivider){
+
+                //Dibuja una linea divisoria entre elementos
+                DividerItemDecoration verticalDecoration = new DividerItemDecoration(recyclerView.getContext(), LinearLayout.VERTICAL);
+                recyclerView.addItemDecoration(verticalDecoration);
             }
 
             PlaceholderContent placeholderContent = new PlaceholderContent(getResources(),
